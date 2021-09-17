78b339ce75ee67a6aa5a578ec98385269cfd28b2
gabijaatmanaviciute <48480494+gabijaatmanaviciute@users.noreply.github.com>
2021-09-17 18:22:03 +0300

    useRef

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,23 +1,45 @@
 import Card from "./Card";
 import Button from "./Button";
 import styles from "./ErrorModal.module.css";
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Backdrop = (props) => {
+  return <div className={styles.backdrop} onClick={props.onErrorHandler} />;
+};
+
+const Overlay = (props) => {
+  return (
+    <Card className={styles.modal}>
+      <header className={styles.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={styles.content}>
+        <p>{props.message}</p>
+      </div>
+      <footer className={styles.actions}>
+        <Button onClick={props.onErrorHandler}>OK</Button>
+      </footer>
+    </Card>
+  );
+};
 
 const ErrorModal = (props) => {
   return (
-    <div>
-      <div className={styles.backdrop} onClick={props.onErrorHandler} />
-      <Card className={styles.modal}>
-        <header className={styles.header}>
-          <h2>{props.title}</h2>
-        </header>
-        <div className={styles.content}>
-          <p>{props.message}</p>
-        </div>
-        <footer className={styles.actions}>
-          <Button onClick={props.onErrorHandler}>OK</Button>
-        </footer>
-      </Card>
-    </div>
+    <React.Fragment>
+      {ReactDOM.createPortal(
+        <Backdrop onErrorHandler={props.onErrorHandler} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <Overlay
+          onErrorHandler={props.onErrorHandler}
+          title={props.title}
+          message={props.message}
+        />,
+        document.getElementById("overlay-root")
+      )}
+    </React.Fragment>
   );
 };
 
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,71 +1,72 @@
-import { useState } from "react";
+import React, { useState, useRef } from "react";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
 import ErrorModal from "../UI/ErrorModal";
 import styles from "./AddUser.module.css";
 
 const AddUser = (props) => {
-  const [enteredUsername, setEnteredUsername] = useState("");
-  const [enteredUserAge, setEnteredUserAge] = useState("");
-  const [error, setError] = useState();
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
 
-  const enterUsernameHandler = (e) => {
-    setEnteredUsername(e.target.value);
-  };
-  const enterUserAgeHandler = (e) => {
-    setEnteredUserAge(e.target.value);
-  };
+  const [error, setError] = useState();
 
   const addUserHandler = (e) => {
     e.preventDefault();
-
-    if (enteredUsername.trim().length === 0 || enteredUserAge.trim().length === 0) {
-        setError({
-            title: "Invalid input",
-            message: "Name and age field cannot be empty."
-        });
-        return;
+    const enteredName = nameInputRef.current.value;
+    const enteredAge = ageInputRef.current.value;
+    if (
+      enteredName.trim().length === 0 ||
+      enteredAge.trim().length === 0
+    ) {
+      setError({
+        title: "Invalid input",
+        message: "Name and age field cannot be empty.",
+      });
+      return;
     }
-    if (+enteredUserAge < 0) {
-        setError({
-            title: "Invalid age",
-            message: "Age cannot be smaller than 0."
-        });
-        return;
+    if (+enteredAge < 0) {
+      setError({
+        title: "Invalid age",
+        message: "Age cannot be smaller than 0.",
+      });
+      return;
     }
-    props.onAddUser(enteredUsername, enteredUserAge);
-
-    setEnteredUsername("");
-    setEnteredUserAge("");
+    props.onAddUser(enteredName, enteredAge);
+    nameInputRef.current.value = '';
+    ageInputRef.current.value = '';
   };
 
   const errorHandler = () => {
-      setError(null);
-  }
+    setError(null);
+  };
 
   return (
-    <div>
-      {error && <ErrorModal title={error.title} message={error.message} onErrorHandler={errorHandler} />}
+    <React.Fragment>
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onErrorHandler={errorHandler}
+        />
+      )}
       <Card className={styles.input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor="username">User name:</label>
           <input
             id="username"
             type="text"
-            value={enteredUsername}
-            onChange={enterUsernameHandler}
+            ref={nameInputRef}
           />
           <label htmlFor="userage">User age:</label>
           <input
             id="userage"
             type="number"
-            value={enteredUserAge}
-            onChange={enterUserAgeHandler}
+            ref={ageInputRef}
           />
           <Button type="submit">Add User</Button>
         </form>
       </Card>
-    </div>
+    </React.Fragment>
   );
 };
 
